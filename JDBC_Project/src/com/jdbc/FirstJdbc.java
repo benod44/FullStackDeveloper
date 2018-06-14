74cea2ee53edf298d92a748379c7daa5e57e4c7e
rawalb <rawalb@IBM927-R90FYU9Y>
2018-06-14 18:58:52 -0500

    //first test

---
+++ JDBC_Project/src/com/jdbc/FirstJdbc.java
@@ -0,0 +1,16 @@
+package com.jdbc;
+
+public class FirstJdbc {
+	
+	public static void main(String [] args){
+		
+		for(int i =1; i<=40; i++){
+			
+			System.out.print(" "+i);
+			
+			
+		}
+		
+	}
+
+}
