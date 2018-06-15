40cc298e8ffe087ad892bd147322f3d0e137bd3c
binod rawal <binod.batech@gmail.com>
2018-06-15 10:01:48 -0500

    //jdbc through Scanner class

--- Test/src/com/jdbc/PracticeJDBC.java
+++ Test/src/com/jdbc/PracticeJDBC.java
@@ -5,57 +5,77 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class PracticeJDBC {
 
-	public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, InterruptedException   {
 		
+	//loading driver
 		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//register the driver
 		Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@Localhost:1521:xe", "system", "1234");
 		
-		//table creation 
-		String q1 = "create table emp (eid number, ename varchar2(10), esal number)";
-		
+		//to execute the query
 		Statement statement = connection.createStatement();
-		int a =statement.executeUpdate(q1);
-		System.out.println("table created "+a);
 		
-		//values inserted into the emp table
-		String q2 = "INSERT INTO emp values (444, 'ben', 7000)";
-		String q3 = "INSERT INTO emp values(888, 'hen', 4000)";
-		int x =statement.executeUpdate(q2);
-		statement.executeUpdate(q3);
-		System.out.println("record inserted "+x);
+		//scanner to read input
+		Scanner sc= new Scanner(System.in);
 		
-		//updating emp table
-		String q4 = "update  emp SET esal= esal+2000 where eid=888";
-		String q5 = "update  emp SET ename='den' where eid=888";
-		statement.executeUpdate(q4);
-		int n=	statement.executeUpdate(q5);
-		System.out.println("row updated "+n);
-		
-		//retriving data from emp table
-		String q6 = "select *from emp";
-		
-		 ResultSet resultSet = statement.executeQuery(q6);
-		 
-		 while(resultSet.next()){
-			 
-			 System.out.println(resultSet.getInt(1)+"----"+resultSet.getString(2)+"--"+resultSet.getInt(3));
-			 
-			 
-		 }
-		 
-		 Thread.sleep(10000);
-		 
-		 String q7 = "drop table emp";
-		 int d =statement.executeUpdate(q7);
-		 System.out.println("table dropped "+d);
+		System.out.println("create a table name: ");
+		String tname = sc.next();
 		
 		
+		int a = statement.executeUpdate("create table "+tname+"(eid number, ename varchar2(10), esal number)");
+		
+		System.out.println("table created sucessfully "+a);
+		
+		//inserting values into the table
+		System.out.println();
+		
+		System.out.println("inserting values in the table");
+		
+		while(true){
+			System.out.println("input the employee id:");
+			int eid = sc.nextInt();
+			
+			System.out.println("input employee name:");
+			String ename = sc.next();
+			
+			System.out.println("input the employee salary");
+			int esal = sc.nextInt();
+			
+			int b = statement.executeUpdate("INSERT INTO emp values ("+eid+", '"+ename+"' ,"+esal+")");
+			
+			System.out.println("records inserted sucessfully "+ b);
+			
+			System.out.println("do you want to ennter one more records yes/no? ");
+			String answer = sc.next();
+			
+			if(answer.equals("no")){
+				break;
+			}
+		}
+		
+		System.out.println("select table to retrive info");
+		String db = sc.next();
 		
 		
-				
+		ResultSet resultSet = statement.executeQuery("select *from "+db);
+		while (resultSet.next()){
+			System.out.println(resultSet.getInt(1)+"----"+resultSet.getString(2)+"----"+resultSet.getInt(3));
+		}
+		
+		Thread.sleep(10000);
+		
+		System.out.println("enter the table name to drop");
+		String drop = sc.next();
+		
+		int d = statement.executeUpdate("drop table "+drop);
+		System.out.println("table dropped sucessfully "+d);
+		
+		
 	}
 
 }
